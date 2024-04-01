8a962ed96241ef188cca639cd3aaa5f7133bccb2
Matheus Silveira <matheus.silveiraw@gmail.com>
2024-04-01 20:38:28 -0300

    first commit

---
+++ Desktop/programação/codi academy tv/Curso Intensivo de React JS  Aprenda em 1H e Crie 2 Apps do Zero/Projeto 1/hello/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+function HellowWorld() {
+    return <div>Hello World</div>
+}
+
+ReactDOM.render(
+    <HellowWorld/>,
+    document.getElementById('root')
+);
+
